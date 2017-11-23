061649560690d4cac2041ab0e45de92d9ac7641d
Aliya Tulyakova <aliyatulyakova@Darryls-MacBook-Pro-8.local>
2017-11-23 14:23:30 +0000

    MagentoModule commit

---
+++ app/code/local/SmashingMagazine/Model/Resource/Setup.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aliyatulyakova
+ * Date: 16/10/2017
+ * Time: 09:22
+ */
+
+class SmashingMagazine_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup{
+
+}
